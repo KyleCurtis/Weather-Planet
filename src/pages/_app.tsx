a450b52f353b3dd9d68cba36dce784504595886b
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-04-01 22:30:01 -0400

    Install and setup deps
    Dark mode via next-themes is setup in this step.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ThemeProvider enableSystem={true} attribute="class">
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ThemeProvider>
   );
 }
